3d66f66873f9949081427f71d635192380c92318
Dineshkumar314 <103531270+Dineshkumar314@users.noreply.github.com>
2022-06-03 00:11:30 +0530

    Add files via upload

---
+++ Header Navigation JS/Header navigation.js
@@ -0,0 +1,13 @@
+  function home(){
+      alert("Welcome to shayan");
+  }
+  function about(){
+    alert("Welcome to shayan. We are software professionals");
+}
+  function openForm() {
+    document.getElementById("myForm").innerHTML = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").innerHTML = "none";
+  }
